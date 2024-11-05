6562d191d22d63c977a559764478fa654c864b28
tamyadav31 <tamyadav31@gmail.com>
2024-11-05 15:31:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+
+from chains import Chain
+from portfolio import Portfolio
+from utils import clean_text
+
+
+def create_streamlit_app(llm, portfolio, clean_text):
+    st.title("📧 Cold Mail Generator")
+    url_input = st.text_input("Enter a URL:", value="https://jobs.nike.com/job/R-43533?from=job%20search%20funnel")
+    name = st.text_input("Enter your name:", "Tamanna Yadav")
+    designation = st.text_input("Enter your designation:", "Business Development Executive")
+    company = st.text_input("Enter your company name:", "XYZ")
+
+    submit_button = st.button("Submit")
+
+    if submit_button:
+        try:
+            loader = WebBaseLoader([url_input])
+            data = clean_text(loader.load().pop().page_content)
+            portfolio.load_portfolio()
+            jobs = llm.extract_jobs(data)
+            for job in jobs:
+                skills = job.get('skills', [])
+                links = portfolio.query_links(skills)
+                email = llm.write_mail(job, links , name, designation, company)
+                st.code(email, language='markdown')
+        except Exception as e:
+            st.error(f"An Error Occurred: {e}")
+
+
+if __name__ == "__main__":
+    chain = Chain()
+    portfolio = Portfolio()
+    st.set_page_config(layout="wide", page_title="Cold Email Generator", page_icon="📧")
+    create_streamlit_app(chain, portfolio, clean_text)
